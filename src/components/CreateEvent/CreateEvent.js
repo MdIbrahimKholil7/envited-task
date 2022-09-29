df049cf4d0ec5c428a852aee9866a7285124ac5e
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-09-29 23:49:30 +0600

    date range add

--- src/components/CreateEvent/CreateEvent.js
+++ src/components/CreateEvent/CreateEvent.js
@@ -1,9 +1,63 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { BiCalendar } from 'react-icons/bi';
+import { format } from 'date-fns'
+import { DateRange } from 'react-date-range';
 const CreateEvent = () => {
+    const [open, setOpen] = useState(false)
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: new Date(),
+            key: "selection",
+        }
+    ])
+    // console.log(format(date[0].startDate, 'PP').split(',')[0].split(' ').join('').reverse())
+    const startDate=format(date[0].startDate, 'PP').split(',')[0].split(' ').reverse()
+    console.log(startDate)
+    const endDate=format(date[0].endDate, 'PP').split(',')[0].split(' ').reverse()
     return (
-        <div>
-            <h1>This is create </h1>
+        <div className='bg-[#F6F2FF] min-h-screen'>
+            <div className='max-w-[745px] mx-auto'>
+                <h2 className='text-center text-[50px] py-20 font-bold text-[#240D57]'>Schedule a event</h2>
+                <div>
+                    <div>
+                        <div 
+                        className='flex items-center gap-5'
+                        >
+                            <BiCalendar 
+                            className='text-[26px] text-[#8456EC]'
+                            />
+                            <div
+                            className='border-2 border-solid border-[#8456EC] rounded-md py-[10px] px-[21px]'
+                            >
+                                {/* <div></div>
+                                <div></div> */}
+                                <span 
+                                className='
+                                text-[#4F4F4F] font-bold
+                                '>
+                                    {
+                                        `${startDate} to ${endDate}`
+                                    }
+                                </span>
+                            </div>
+                            {
+                                open && <span className='absolute z-10 top-[46px] md:left-[-136px] left-[-90px]'>
+                                    <DateRange
+                                        editableDateInputs={true}
+                                        onChange={(item) => setDate([item.selection])}
+                                        moveRangeOnFirstSelection={false}
+                                        ranges={date}
+                                        minDate={new Date()}
+                                    // maxDate={date[0].startDate}
+
+                                    />
+                                </span>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import btnImg from '../../assets/btn.png'
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     return (
         <div>
             <div className="hero min-h-screen bg-[#F6F2FF] pt-8">
@@ -12,6 +12,7 @@ const Home = () => {
                     <div>
                         <img src={landingImg} alt='homeImg' />
                         <button
+                            onClick={() => navigate('/create')}
                             className="bg-gradient-to-r from-[#8456EC] to-[#E87BF8] py-[16px] rounded-[10px] block lg:hidden mx-auto"
                         >
                             <img className='px-[21px]' src={btnImg} alt="" />
@@ -31,10 +32,10 @@ const Home = () => {
                             <span
                                 className='block md:text-[64px]  text-[36px] text-[#240D57] font-bold leading-[100%]'>had events.</span>
                         </div>
-                        <p className="py-6 text-[#4F4F4F] text-center lg:text-right text-[16px] md:text-[24px]  md:px-40 lg:px-0 lg:w-full lg:pl-40">Easily host and share events with your friends across any social media.</p>
+                        <p className="py-6 text-[#4F4F4F] text-center lg:text-right text-[16px] md:text-[24px]  md:px-40 lg:px-0 lg:w-full lg:pl-80">Easily host and share events with your friends across any social media.</p>
                         <div className='flex justify-end items-center'>
                             <button
-                            onClick={()=>navigate('/create')}
+                                onClick={() => navigate('/create')}
                                 className="bg-gradient-to-r from-[#8456EC] to-[#E87BF8] py-[16px] rounded-[10px] hidden lg:block"
                             >
                                 <img className='px-[21px]' src={btnImg} alt="" />
